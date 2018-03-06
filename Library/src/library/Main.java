b09cfbd0ccc7992e58df6cf854cc735cfe78f42b
josh <jreiss@oswego.edu>
2018-03-06 13:13:30 -0500

    save

--- Library/src/library/Main.java
+++ Library/src/library/Main.java
@@ -15,6 +15,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("run me");
         // TODO code application logic here
     }
     
